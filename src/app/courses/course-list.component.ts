4f1d0c66725117d2c7ddc900ae9dd07a1c43363c
Italo1994 <italohortiz2015@gmail.com>
2021-05-11 22:57:21 -0300

    Criando Tabela de Cursos Adicionando a tabela e listando os cursos

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,9 +1,36 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
 
 @Component({
   selector: "app-course-list",
   templateUrl: "./course-list.component.html"
 })
-export class CourseListComponent {
-  //course: Course[] = [];
+export class CourseListComponent implements OnInit {
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 25,
+        name: "Angular",
+        imageUrl: "",
+        price: 120.5,
+        code: "ANG - 20NG",
+        duration: 40,
+        rating: 10,
+        releaseDate: "April, 22, 2021"
+      },
+
+      {
+        id: 20,
+        name: "React",
+        imageUrl: "",
+        price: 150.5,
+        code: "REC - 25NG",
+        duration: 50,
+        rating: 10,
+        releaseDate: "May, 7, 2021"
+      }
+    ];
+  }
 }
